8d80232cce9d1936c5400ee38a5528145f391fef
RM25BAM <98143256+RM25BAM@users.noreply.github.com>
2022-08-15 15:15:55 -0400

    Delete predict_iphone_price.py

--- predict_iphone_price.py
+++
@@ -1,9 +0,0 @@
-import pandas
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-data = pandas.read_csv('iphone_price.csv')
-plt.scatter(data['version'], data['price'])
-plt.show()
-model = LinearRegression()
-model.fit(data[['version']], data[['price']])
-print(model.predict([[30]]))
